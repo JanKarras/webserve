52c612681d4ee11169c337993b78c4b4b49c6720
atoepper <atoepper@student.42wolfsburg.de>
2025-02-19 10:49:21 +0100

    added rudimental parsing structure

---
+++ server/src/httpParser/http_request.cpp
@@ -0,0 +1,48 @@
+#include "../include/webserv.hpp"
+
+int parseHttpRequestLine(HttpRequest &req, const std::string &data)
+{
+	size_t p = req.pos;
+	req.buffer.append(data);
+	req.content_length = req.buffer.length();
+	
+	enum {
+		START,
+		METHOD,
+		URI,
+		VERSION,
+		DONE,
+		ERROR
+	} state;
+	state = req.parseState;
+
+	for (p; p < req.content_length; p++)
+	{
+		switch (state)
+		{
+			case START:
+
+			case METHOD:
+
+			case URI:
+
+			case VERSION:
+
+		}
+	}
+	req.pos = p;
+	req.parseState = state;
+	if (req.parseState == DONE)
+	{
+		req.parseState = START;
+		req.state = HEADERS;
+		return 0;
+	}
+	req.state = REQUEST_LINE;
+	return SUCCESS;
+}
+
+void parseHttpHeaderLine(HttpRequest &req, const std::string &data)
+{
+
+}
