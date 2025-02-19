f2991d2d8134cd4b8e5fb2f333a511fde6d307b3
atoepper <atoepper@student.42wolfsburg.de>
2025-02-19 13:36:44 +0100

    refined parse functions

--- server/src/httpParser/http_request.cpp
+++ server/src/httpParser/http_request.cpp
@@ -1,22 +1,13 @@
 #include "../include/webserv.hpp"
 
-int parseHttpRequestLine(HttpRequest &req, const std::string &data)
+int parseHttpRequestLine(HttpRequest &req)
 {
 	size_t p = req.pos;
-	req.buffer.append(data);
-	req.content_length = req.buffer.length();
+	RequestLineState state = static_cast<RequestLineState>(req.parseState);
 	
-	enum {
-		START,
-		METHOD,
-		URI,
-		VERSION,
-		DONE,
-		ERROR
-	} state;
-	state = req.parseState;
-
-	for (p; p < req.content_length; p++)
+	size_t buffer_length = req.buffer.length();
+	
+	for (p; p < buffer_length; p++)
 	{
 		switch (state)
 		{
@@ -28,21 +19,80 @@ int parseHttpRequestLine(HttpRequest &req, const std::string &data)
 
 			case VERSION:
 
+			case DONE:
+
 		}
 	}
 	req.pos = p;
-	req.parseState = state;
-	if (req.parseState == DONE)
+	req.parseState = static_cast<int>(state);
+	if (state == DONE)
 	{
-		req.parseState = START;
+		req.parseState = 0;
 		req.state = HEADERS;
+		/* clear buffer, set req.pos to 0 */
 		return 0;
 	}
 	req.state = REQUEST_LINE;
 	return SUCCESS;
 }
 
-void parseHttpHeaderLine(HttpRequest &req, const std::string &data)
+int parseHttpHeaderLine(HttpRequest &req)
 {
+	size_t p = req.pos;
+	size_t buffer_length = req.buffer.length();
+	
+	while (req.state != COMPLETE && !req.buffer.empty()) {
+		std::istringstream stream(req.buffer);  // Create a new stream with the updated buffer
+		std::string line;
 
+		if (std::getline(stream, line)) {
+			if (line == "\r") {
+				if (req.headers.count("Content-Length")) {
+					req.state = BODY;
+				} else {
+					req.state = COMPLETE;
+				}
+			} else {
+				size_t splitPos = line.find(":");
+				if (splitPos != std::string::npos) {
+					std::string key = line.substr(0, splitPos);
+					std::string value = line.substr(splitPos + 2);
+					req.headers[key] = value;
+				}
+			}
+			req.buffer.erase(0, line.size() + 1);  // Erase the processed line and newline
+		} else {
+			break; // In case getline fails (empty buffer)
+		}
+	}
+	return 0; // or appropriate return value
+}
+
+/* parse URI? */
+
+int parseHttpBody(HttpRequest &req)
+{
+	size_t p = req.pos;
+	size_t buffer_length = req.buffer.length();
+
+	req.body.append(req.buffer);
+	if (req.body.size() >= strtoul(req.headers["Content-Length"].c_str(), NULL, 10))
+	{
+		req.buffer.clear();
+		req.state = COMPLETE;
+	}
+}
+
+void	parseHttpRequest(HttpRequest &req, std::string &data)
+{
+	req.buffer.append(data);
+	switch (req.state)
+	{
+		case REQUEST_LINE:
+			parseHttpRequestLine(req);
+		case HEADERS:
+			parseHttpHeaderLine(req);
+		case BODY:
+			parseHttpBody(req);
+	}
 }
