62560a6c1a9771aecb5aa0524af140fbd4d1131e
atoepper <atoepper@student.42wolfsburg.de>
2025-02-21 15:26:12 +0100

    added routs for indexhtml

--- server/src/helper/initRoutes.cpp
+++ server/src/helper/initRoutes.cpp
@@ -2,6 +2,12 @@
 
 void initGET(std::map<std::string, void (*)(HttpRequest &, HttpResponse &)> &get) {
 	get["/"] = handleHome;
+	get["/assets/img/42Wolfsburg_Logo_ver_pos_black.pdf"] = handleHome;
+	get["/assets/img/atoepper"] = handleHome;
+	get["/assets/img/jkarras"] = handleHome;
+	get["/assets/img/rmatthes"] = handleHome;
+	get["/assets/css/index_style.css"] = handleIndexSstyle;
+	get["/assets/js/index.js"] = handleHome;
 }
 
 void initPOST(std::map<std::string, void (*)(HttpRequest &, HttpResponse &)> &post) {
